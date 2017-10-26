85293a73956d3ec4e33a3f7f715864ba748cf2e3
Alexander Vincent <AlexV9582@yahoo.com>
2017-10-26 18:35:54 -0400

    Redone for React

---
+++ bandmate/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumbotron = () => (
+
+    <div>
+        <div class="jumbotron">
+            <h1>BandMate</h1>
+            <p>Your brand new set list creator</p>
+        </div>
+    </div>
+
+);
+
+export default Jumbotron;
