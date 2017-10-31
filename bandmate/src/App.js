f5d40fb8a6d76452835a8edf7d3618a23ef8cd10
Alexander Vincent <AlexV9582@yahoo.com>
2017-10-31 14:26:37 -0400

    with drag and drop started

--- bandmate/src/App.js
+++ bandmate/src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Jumbotron from "./components/Jumbotron"
+import Box from './components/Draggable/Draggable'
 
+
+
+
+const arraySongs= ["Don't worry be happy ",'Californication','La-la-la-la','Wall',"It's my life",'Under my thumb']
 class App extends Component {
+
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -14,9 +23,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Box songs={arraySongs} />
       </div>
     );
   }
+
+
 }
 
 export default App;
